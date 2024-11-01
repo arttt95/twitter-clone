dda8f2a6f1a4286d81e02673bcf25e843e92a858
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-01 11:06:21 -0300

    01NOV24 - Aula 535 - Twitter Clone (Registrando Usuários).

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -19,8 +19,55 @@ class IndexController extends Action {
 
     public function inscreverse() {
 
+        $this->view->usuario = [
+            'nome' => $_POST[''],
+            'email' => $_POST[''],
+            'senha' => $_POST['']
+        ];
+
+        $this->view->erroCadastro = false;
+
         $this->render('inscreverse');
 
+    }
+
+    public function registrar() {
+
+        /*  echo '<pre>';
+            print_r($_POST);
+            echo '</pre>';    */
+
+        // Receber os dados do formulário
+        $usuario = Container::getModel('Usuario');
+
+        $usuario->__set('nome', $_POST['nome']);
+        $usuario->__set('email', $_POST['email']);
+        $usuario->__set('senha', $_POST['senha']);
+
+        /*  echo '<pre>';
+            print_r($usuario);
+            echo '</pre>';    */
+
+        if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) { //Sucesso
+
+            $usuario->salvar();
+
+            $this->render('cadastro');
+
+        } else { // Erro
+
+            $this->view->usuario = [
+                'nome' => $_POST['nome'],
+                'email' => $_POST['email'],
+                'senha' => $_POST['senha']
+            ];
+
+            $this->view->erroCadastro = true;
+            
+            $this->render('inscreverse');
+
+        }
+        
     }
 
 }
---
+++ App/Models/Usuario.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Usuario extends Model {
+
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+
+    public function __get($attr) {
+        return $this->$attr;
+    }
+
+    public function __set($attr, $value) {
+        $this->$attr = $value;
+    }
+
+    // Salvar
+    public function salvar() {
+
+        $query = '
+            INSERT INTO
+                usuarios(nome, email, senha) values (:nome, :email, :senha)
+        ';
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome', $this->__get('nome'));
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', $this->__get('senha')); //md5() -> hash de 32 carac
+        $stmt->execute();
+
+        return $this;
+
+    }
+
+    // Realizar validação para verificar se um cadastro pode ser feito
+    public function validarCadastro() {
+        $valido = true;
+
+        if(strlen($this->__get('nome')) < 3) {
+            $valido = false;
+        }
+
+        if(strlen($this->__get('email')) < 2) {
+            $valido = false;
+        }
+
+        if(strlen($this->__get('senha')) < 2) {
+            $valido = false;
+        }
+
+        return $valido;
+    }
+
+    // Recuperar um usuário por e-mail
+    public function getUsuarioPorEmail() {
+
+        $query = '
+            SELECT
+                nome, email
+            FROM
+                usuarios
+            WHERE
+                email = :email
+        ';
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    }
+
+}
+
+?>
