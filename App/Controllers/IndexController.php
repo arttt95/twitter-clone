99f6c8f640e5d0e78d7e539d7eccbd2b6407a67d
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-02 22:39:14 -0300

    02NOV24 - Aula 537 - Incluindo criptografia MD5 para senha de usuários.

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -44,7 +44,7 @@ class IndexController extends Action {
 
         $usuario->__set('nome', $_POST['nome']);
         $usuario->__set('email', $_POST['email']);
-        $usuario->__set('senha', $_POST['senha']);
+        $usuario->__set('senha', md5($_POST['senha']));
 
         /*  echo '<pre>';
             print_r($usuario);
